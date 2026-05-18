aa796a3c1a713e234fd35097e065fd3834673035
giankfb <giankfb@users.noreply.github.com>
2026-05-18 19:48:15 -0300

    Add files via upload

--- js/modules/dashboard.js
+++ js/modules/dashboard.js
@@ -1,5 +1,39 @@
 async function carregarDashboard() {
+
   const eventos = await api('listarEventos');
 
-  console.log(eventos);
+  renderizarEventos(eventos);
+}
+
+function renderizarEventos(eventos){
+
+  const container = document.getElementById('eventos');
+
+  container.innerHTML = '';
+
+  eventos.forEach(evento => {
+
+    container.innerHTML += `
+      <div class="card-evento">
+
+        <h3>${evento.CLIENTE}</h3>
+
+        <p><strong>Data:</strong> ${evento.DATA}</p>
+
+        <p><strong>Tipo:</strong> ${evento.TIPO}</p>
+
+        <p><strong>Telefone:</strong> ${evento.TELEFONE}</p>
+
+        <p><strong>Valor:</strong> R$ ${evento.VALOR}</p>
+
+        <p><strong>Sinal:</strong> R$ ${evento.SINAL}</p>
+
+        <p><strong>Restante:</strong> R$ ${evento.RESTANTE}</p>
+
+        <p><strong>Status:</strong> ${evento.STATUS}</p>
+
+      </div>
+    `;
+  });
+
 }
