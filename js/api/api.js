ca99edb53b2da6558989503bfb7f022c29cfef1f
giankfb <giankfb@users.noreply.github.com>
2026-05-18 20:16:33 -0300

    Add files via upload

--- js/api/api.js
+++ js/api/api.js
@@ -1,9 +1,20 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzsOmtv0V5g5_1_HNjJ69tiNU764VoKELJCu4iIIA51hGJSmoKMCpJOxvIel1JNNXYc/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycby-cEZgZqIMUtwVsrKlDlFnOTJedVxHfYBBiTtJM3hCvJTbE5sZVZpkf4D_fWknfX8c/exec";
 
-async function api(action) {
+async function api(action){
+
   const response = await fetch(
     `${API_URL}?action=${action}`
   );
+
+  return await response.json();
+}
+
+async function post(dados){
+
+  const response = await fetch(API_URL,{
+    method:'POST',
+    body:JSON.stringify(dados)
+  });
 
   return await response.json();
 }
