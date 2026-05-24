7b2a89f7dac4b04bb7494d99d1077b23be81b85a
giankfb <giankfb@users.noreply.github.com>
2026-05-24 19:06:10 -0300

    Add files via upload

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 const APP_VERSION = '1.1.1';
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbxsxQZEz5sQ4brCKkG5oJFgo7LsbH05U634L96-ZJg9Dk2l9u7AdXwjPILKTOkzspnw/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycby4-B4FNBb-xT97CwAEecexSeBKgi6_UfRjoJNZVNX-JNh_dkuY-b9e9LwYl2t1f9se/exec';
